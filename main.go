b0449dc2502e0996be4088c14d1ee935301e6f67
takumi616 <ohohyeah321@icloud.com>
2024-02-06 01:42:57 +0900

    Incorporate errgroup package into implementation of http server.

--- main.go
+++ main.go
@@ -1,19 +1,52 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"golang.org/x/sync/errgroup"
 )
 
-func main() {
-	err := http.ListenAndServe(
-		":8000",
-		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+func run(ctx context.Context) error {
+	server := &http.Server{
+		Addr: ":8000",
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "Request Path: %s", r.URL.Path[1:])
 		}),
-	)
+	}
+
+	//Run http server in another groutine
+	//to stop it from external action
+	//like sending cancel signal
+	eg, ctx := errgroup.WithContext(ctx)
+	eg.Go(func() error {
+		err := server.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			log.Printf("Failed to run http server: %v", err)
+			return err
+		}
+		return nil
+	})
+
+	//Wait for cancel notification 
+	//ctx is canceled when groutine running http server returns error
+	<-ctx.Done()
+	err := server.Shutdown(context.Background())
 	if err != nil {
-		log.Fatalf("Failed to run http server: %v", err)
+		log.Printf("Failed to shutdown http server: %v", err)
+	}
+
+	//Return goroutine's response (err or nil) 
+	return eg.Wait()
+}
+
+func main() {
+	//Create run() to make it easy testing http server
+	err := run(context.Background())
+	if err != nil {
+		log.Printf("Main process error: %v", err)
+	} else {
+		log.Println("Main process finished successfully.")
 	}
 }
